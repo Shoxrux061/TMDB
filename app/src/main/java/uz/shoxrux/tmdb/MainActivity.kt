cdb2f247f2e04cf6428f7bc597602af3f358da57
Shoxrux061 <shoxruxurmanov2@gmail.com>
2025-08-22 19:03:36 +0500

    feat: appComponents

--- app/src/main/java/uz/shoxrux/tmdb/MainActivity.kt
+++ app/src/main/java/uz/shoxrux/tmdb/MainActivity.kt
@@ -3,7 +3,6 @@ package uz.shoxrux.tmdb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import uz.shoxrux.core.ui.theme.AppTheme
 
@@ -11,7 +10,7 @@ import uz.shoxrux.core.ui.theme.AppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             AppTheme {
 
