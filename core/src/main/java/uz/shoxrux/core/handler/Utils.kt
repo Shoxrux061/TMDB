eca214ce8b3bbe07a9dc7c7d2282f9e05f40e9fb
Shoxrux061 <shoxruxurmanov2@gmail.com>
2025-09-23 16:05:48 +0500

    refactor safeApiCall

--- core/src/main/java/uz/shoxrux/core/handler/Utils.kt
+++ core/src/main/java/uz/shoxrux/core/handler/Utils.kt
@@ -1,7 +1,11 @@
 package uz.shoxrux.core.handler
 
+import android.util.Log
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
 
 inline fun <T, R> safeApiCall(
     crossinline apiCall: suspend () -> T,
@@ -10,7 +14,17 @@ inline fun <T, R> safeApiCall(
     try {
         val response = apiCall()
         emit(NetworkResult.Success(mapper(response)))
+    } catch (e: HttpException) {
+        Log.e("safeApiCall", "HTTP ${e.code()} ${e.message()}")
+        emit(NetworkResult.Error(AppError.Server(e.message())))
+    } catch (e: IOException) {
+        Log.e("safeApiCall", "Network error: ${e.message}")
+        emit(NetworkResult.Error(AppError.Server(e.message)))
+    } catch (e: Throwable) {
+        if (e is CancellationException) throw e
+        emit(NetworkResult.Error(AppError.Unknown(e)))
     } catch (e: Exception) {
-        emit(NetworkResult.Error(ErrorHandler.parse(e)))
+        Log.e("safeApiCall", "Unknown error: ${e.message}", e)
+        emit(NetworkResult.Error(AppError.Unknown()))
     }
 }
